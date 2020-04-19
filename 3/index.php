53220f5c1d4bc3e45f7e4a621df18b329d87fcaa
RustamjonUsmonov11-811 <43405405+RustamjonUsmonov@users.noreply.github.com>
2020-04-19 09:50:31 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -1,6 +1,6 @@
 <form method="post" action="sec.php">
     <p><h2>Введите ваш текст:</h2></p>
-    <p><b>Например: First sentence/Happy birthday/Bad guys always lose</b></p>
+    <p><b>Например: First sentence is  first sentence/Happy birthday Mr.President/Bad guys always lose</b></p>
     <p><textarea rows="10" cols="45" name="textarea"></textarea></p>
     <p><input type="submit" value="Отправить"></p>
 </form>
