a48a87620c273c9a48e837fcacd1a009a0ae8fe3
RustamjonUsmonov11-811 <43405405+RustamjonUsmonov@users.noreply.github.com>
2020-04-21 22:16:06 +0300

    Update index.php

--- 2/index.php
+++ 2/index.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Converting text</title>
-</head>
-<body>
 <form action="1.php" method="post" >
     <fieldset>
         <legend>Type something</legend>
-
         String: <input type="text" name="sometext" string="string"><br><br/>
-
         Submit:     <input type="submit" name="done" value="Send">
-
     </fieldset>
 </form>
-</body>
-</html>
+<?php
